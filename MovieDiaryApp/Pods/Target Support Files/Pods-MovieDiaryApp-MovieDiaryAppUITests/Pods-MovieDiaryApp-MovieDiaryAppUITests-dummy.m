7ce09e1c187be9ce8b43cd1ff65c25f8ba672b61
ssemih42 <ssemih4238@gmail.com>
2020-01-06 22:44:08 +0300

    Pod init

---
+++ MovieDiaryApp/Pods/Target Support Files/Pods-MovieDiaryApp-MovieDiaryAppUITests/Pods-MovieDiaryApp-MovieDiaryAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieDiaryApp_MovieDiaryAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MovieDiaryApp_MovieDiaryAppUITests
+@end
