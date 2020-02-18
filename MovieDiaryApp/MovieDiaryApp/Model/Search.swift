1de8de3a389c6ff2494df5b75e00a5cf93b2e9a3
ssemih42 <ssemih4238@gmail.com>
2020-02-18 13:27:50 +0300

    Hatalar giderildi

--- MovieDiaryApp/MovieDiaryApp/Model/Search.swift
+++ MovieDiaryApp/MovieDiaryApp/Model/Search.swift
@@ -1,27 +1,19 @@
 import ObjectMapper
 import RealmSwift
-
-
 class Search: Object, Mappable  {
     required init?(map: Map) {
-       
     }
-    
     required init() {
         
     }
-    
     override class func primaryKey() -> String? {
         return "imdbID"
     }
-    
 @objc dynamic var title : String?
 @objc dynamic var year : String?
 @objc dynamic var imdbID : String?
 @objc dynamic  var type : String?
 @objc dynamic var poster : String?
-    
-  
     func mapping(map: Map) {
         title <- map["Title"]
         year <- map["Year"]
